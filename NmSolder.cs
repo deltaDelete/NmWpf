34e955a932c7b19c1c885738b01642592a14d318
deltaDelete <40860312+deltaDelete@users.noreply.github.com>
2022-06-08 00:19:42 +0700

    Добавьте файлы проекта.

---
+++ NmSolder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using org.mariuszgromada.math.mxparser;
+using System.Linq;
+
+namespace NmWpf;
+internal class NmSolder {
+    public static List<XY> Solve(string fValue, double a, double b, double h, double y0, int precision) {
+        double x = a;
+        double y = y0;
+        double n = Math.Round((b - a) / h, 1);
+        if (n % 1 != 0) {
+            throw new ArgumentOutOfRangeException(message: "Результат деления отрезка на шаг должен быть целым", null);
+        }
+        List<XY> result = new();
+        result.Add(new(x, y, precision));
+        for (int i = 0; i <= n - 1; i++) {
+            string xString = x.ToString().Replace(',', '.');
+            string yString = y.ToString().Replace(',', '.');
+            string expString = fValue.Replace("x", xString).Replace("y", yString);
+            Expression exp = new(expString);
+            y = y + h * exp.calculate();
+            x = x + h;
+            result.Add(new(
+                x,
+                y,
+                precision
+                ));
+            if (double.IsNaN(x) || double.IsNaN(y)) {
+                throw new Exception("Не число");
+            }
+        }
+        return result;
+    }
+}
+
+public class XY {
+    public double X { get; set; }
+    public double Y { get; set; }
+    public int Precision { get; set; }
+    public XY(double x, double y, int precision = 3) {
+        Precision = precision;
+        X = Math.Round(x, precision);
+        Y = Math.Round(y, precision);
+    }
+    public static explicit operator (double, double)(XY value) => (value.X, value.Y);
+    public static explicit operator XY((double, double) value) => new(value.Item1, value.Item2);
+    public override string ToString() {
+        return $"{Math.Round(X, Precision).ToString()} : {Math.Round(Y, Precision).ToString()}";
+    }
+}
---
+++ Pages/HomePage.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NmWpf.Pages;
+/// <summary>
+/// Логика взаимодействия для HomePage.xaml
+/// </summary>
+public partial class HomePage : Page, INotifyPropertyChanged {
+    public string FValue {
+        get {
+            return FValueBox.Text;
+        }
+        set {
+            FValueBox.Text = value;
+        }
+    }
+    public double AValue {
+        get {
+            return AValueBox.Value;
+        }
+        set {
+            AValueBox.Value = value;
+        }
+    }
+    public double BValue {
+        get {
+            return BValueBox.Value;
+        }
+        set {
+            BValueBox.Value = value;
+        }
+    }
+    public double HValue {
+        get {
+            return HValueBox.Value;
+        }
+        set {
+            HValueBox.Value = value;
+        }
+    }
+    public double Y0Value {
+        get {
+            return Y0ValueBox.Value;
+        }
+        set {
+            Y0ValueBox.Value = value;
+        }
+    }
+    public int PrecisionValue {
+        get {
+            return Convert.ToInt32(PrecisionValueBox.Value);
+        }
+        set {
+            Y0ValueBox.Value = value;
+        }
+    }
+    public List<XY> Result {
+        set {
+            ResultTable.ItemsSource = value;
+        }
+    }
+    public HomePage() {
+        InitializeComponent();
+        DataContext = this;
+        PropertyChanged += HomePage_PropertyChanged;
+    }
+
+    private void HomePage_PropertyChanged(object? sender, PropertyChangedEventArgs e) {
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void Button_Click(object sender, RoutedEventArgs e) {
+        try {
+            Result = NmSolder.Solve(FValue, AValue, BValue, HValue, Y0Value, PrecisionValue);
+        }
+        catch (Exception exc) {
+            WPFUI.Controls.MessageBox mbox = new WPFUI.Controls.MessageBox();
+            mbox.ResizeMode = ResizeMode.NoResize;
+            mbox.Content = "Произошла ошибка,\nпроверьте корректность введенных значений";
+            mbox.ButtonRightName = "Ладно";
+            mbox.ButtonRightAppearance = WPFUI.Common.Appearance.Primary;
+            mbox.ButtonRightClick += (sender, e) => {
+                if (sender is WPFUI.Controls.MessageBox && sender is not null)
+                (sender as WPFUI.Controls.MessageBox).Close();
+            };
+            mbox.ButtonLeftName = "Не ладно";
+            mbox.ButtonLeftAppearance = WPFUI.Common.Appearance.Danger;
+            mbox.ButtonLeftClick += (sender, e) => {
+                if (sender is WPFUI.Controls.MessageBox && sender is not null)
+                    (sender as WPFUI.Controls.MessageBox).Close();
+            };
+            mbox.Show();
+        }
+    }
+}
